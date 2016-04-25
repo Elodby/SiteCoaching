06d99dd0053736b786c99537c932f606194226fb
PcMarie <marie.cuer@hotmail.fr>
2016-04-25 11:25:20 +0200

    Ajout Pop up boostrap

--- index.php
+++ index.php
@@ -275,6 +275,29 @@ $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
     </section>
     <input type="button" value="test" onclick="window.open('http://localhost/SiteCoaching-master/questionnaire.php')">
     <a href="javascript:ouvrirFenetre();"> Test2 </a>
+    <!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal test3 -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>test .</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
     <footer>
         <div class="container">
             <div class="row">
