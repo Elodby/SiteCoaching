1a6d36daf43ad87a6f05a86bf8490ba83d3c9407
PcMarie <marie.cuer@hotmail.fr>
2016-04-25 11:21:07 +0200

    modifications

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=sport', 'root', '');
+$sql = "SELECT * FROM `article`";
+ 
+//Prepare our SELECT statement.
+$statement = $pdo->prepare($sql);
+ 
+//Execute our statement.
+$statement->execute();
+ 
+//Fetch our rows. Array (empty if no rows). False on failure.
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -110,7 +121,17 @@
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Les coatchs </h2>
                     <h3 class="section-subheading text-muted">présentation des différents coatchs</h3>
-                    <a href="pagePresentation.php">Voir plus </a>
+                    <?php
+                    foreach($rows as $row){
+                            echo 'id_article: ' . $row['ID_ARTICLE'] . '<br>';
+                            echo 'id_categorie: ' . $row['ID_CATEGORIE'] . '<br>';
+                            echo 'id_temoignage: ' . $row['ID_TEMOIGNAGE'] . '<br>';
+                            echo 'title: ' . $row['TITLE'] . '<br>';
+                            echo 'description: ' . $row['DESCRIPTION'] . '<br>';
+                            echo 'introduction: ' . $row['INTRODUCTION'] . '<br>';
+                       }
+                   ?>
+                    <a href="pagePresentation.php">Plus</a>
                 </div>
             </div>
             <div class="row text-center">
@@ -178,214 +199,7 @@
             </div>
         </div>
     </section>
-
-    <!-- Clients Aside -->
-    <section class="bg-light-gray" id="partenaires">
-    <aside class="clients">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-6">
-                    <a href="#">
-                        <img src="img/logos/envato.jpg" class="img-responsive img-centered" alt="">
-                    </a>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <a href="#">
-                        <img src="img/logos/designmodo.jpg" class="img-responsive img-centered" alt="">
-                    </a>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <a href="#">
-                        <img src="img/logos/themeforest.jpg" class="img-responsive img-centered" alt="">
-                    </a>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <a href="#">
-                        <img src="img/logos/creative-market.jpg" class="img-responsive img-centered" alt="">
-                    </a>
-                </div>
-            </div>
-        </div>
-    </aside>
-</section>
-    
-    <!-- Contact Section -->
-    <section id="contact">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">nous contacter</h2>
-                    <h3 class="section-subheading text-muted"></h3>
-    <title>Agency - Start Bootstrap Theme</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/agency.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body id="page-top" class="index">
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#lasalle">LA SALLE</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#lescoachs">LES COACHS</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#lescours">LES COURS</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#tarifsplanning">TARIFS / PLANNING</a>
-                    </li>             
-                    <li>
-                        <a class="page-scroll" href="#temoignages">TEMOIGNAGES</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#partenaires">PARTENAIRES</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">NOUS CONTACTER</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
-
-    <!-- Header -->
-    <header>
-        <div class="container">
-            <div class="intro-text">
-                <div class="intro-message">
-                    <h1>Landing Page</h1>
-                    <h3>A Template by Start Bootstrap</h3>
-                    <hr class="intro-divider">
-                    <ul class="list-inline intro-social-buttons">
-                        <li>
-                            <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Facebook</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- Services Section -->
-    <section id="lasalle">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">LA SALLE</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-            </div>
-            <div class="row text-center">
-                
-            </div>
-        </div>
-    </section>
-
-    <!-- Portfolio Grid Section -->
-    <section id="lescoachs" class="bg-light-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Les coachs</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-            </div>
-            <div class="row">
-                
-            </div>
-        </div>
-    </section>
-
-    <!-- Les cours Section -->
-    <section id="lescours">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Les cours</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-            </div>
-            <div class="row">
-
-            </div>
-        </div>
-    </section>
-
-    <!-- About Section -->
-    <section id="tarifsplanning" class="bg-light-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Tarifs / planning</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-            </div>
-            <div class="row">
-                
-            </div>
-        </div>
-    </section>
-
-    <!-- Team Section -->
-    <section id="temoignages">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">temoignages</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-            </div>
-            <div class="row">
-                
-            </div>
-        </div>
-    </section>
-
-    <!-- Clients Aside -->
+<!-- Clients Aside -->
     <section class="bg-light-gray" id="partenaires">
     <aside class="clients">
         <div class="container">
@@ -459,7 +273,8 @@
             </div>
         </div>
     </section>
-
+    <input type="button" value="test" onclick="window.open('http://localhost/SiteCoaching-master/questionnaire.php')">
+    <a href="javascript:ouvrirFenetre();"> Test2 </a>
     <footer>
         <div class="container">
             <div class="row">
@@ -510,3 +325,9 @@
 </body>
 
 </html>
+<script language="javascript">
+function ouvrirFenetre()
+{
+    window.open("http://localhost/SiteCoaching-master/questionnaire.php", "popup", "toolbar=0, location=0, directories=0, status=0, scrollbars=0, resizable=0, copyhistory=0, width=500, height=350,screenX=200,screenY=200");
+}
+</script>
---
+++ questionnaire.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <title>
+            
+        </title>
+    </head>
+    <body>
+    <center>
+        <h1> Questionnaire </h1>
+        <form action="questionnaire.php" method="post">
+            <p>test : <input type="text" name="cellule1" /></p>
+            <p>test : <input type="text" name="cellule2" /></p>
+            <p><input type="submit" value="OK"></p>
+        </form>
+    </center>
+    </body>
+</html>
